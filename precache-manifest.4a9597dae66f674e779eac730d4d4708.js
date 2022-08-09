c898232998d77583daed511fdd0d2a04eb27504d
Aarush Paul <76874133+aarush-paul@users.noreply.github.com>
2022-08-10 00:11:46 +0530

    Updates

---
+++ precache-manifest.4a9597dae66f674e779eac730d4d4708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97abce2ce02b946d9d3a8ad0eb41675c",
+    "url": "/symptom-checker/index.html"
+  },
+  {
+    "revision": "0254f64f64aecd53535f",
+    "url": "/symptom-checker/static/css/main.3e65e04f.chunk.css"
+  },
+  {
+    "revision": "60a6e6d64a597cd501a0",
+    "url": "/symptom-checker/static/js/2.e4700786.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/symptom-checker/static/js/2.e4700786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0254f64f64aecd53535f",
+    "url": "/symptom-checker/static/js/main.eb1336b4.chunk.js"
+  },
+  {
+    "revision": "69360b23eb4b10ae857d",
+    "url": "/symptom-checker/static/js/runtime-main.3c79473f.js"
+  },
+  {
+    "revision": "7c583c79e665fe726210608b348137e1",
+    "url": "/symptom-checker/static/media/logo.7c583c79.svg"
+  }
+]);
